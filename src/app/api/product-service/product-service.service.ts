41f37071e5be7f4974d07ce2ad78b1cd15d747a4
ChristianCage7 <43277630+ChristianCage7@users.noreply.github.com>
2024-08-26 21:15:47 -0400

    Add files via upload

---
+++ src/app/api/product-service/product-service.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Products } from 'src/app/models/product';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductServiceService {
+
+  private product_list Products[][
+    {
+      "id": 1;
+      "sku": 111;
+      "nombre": Pelota;
+      "compania": Peloton;
+      "stock": 3;
+      "precio": 9990;
+    }
+  ]
+
+  }
+
+  constructor() { }
+}
---
+++ src/app/models/product.ts
@@ -0,0 +1,10 @@
+export interface Products {
+
+    id: number;
+    sku: number;
+    nombre: string;
+    compania: string;
+    stock: number;
+    precio: number;
+
+}
